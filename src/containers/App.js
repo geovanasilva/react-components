8a710e192fd6fda9b5f1554e32f96bff8e996066
Geovana Silva <sts.geovana@gmail.com>
2020-01-26 20:59:13 -0300

    feat(init): starting project

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <p>React Component</p>
+            </div>
+        )
+    }
+}
+
+export default App
